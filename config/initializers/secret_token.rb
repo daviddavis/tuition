40eb7055901dc3ef3c93c275a3d4312f5e3f86f5
David Davis <davidd@scimedsolutions.com>
2012-03-03 11:30:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuition::Application.config.secret_token = '5755d59ebb9eec6524a8547e0670acff872cf13bc38fe9d1a2dae1b3d15c7cf6645140b29f4e8d8eb79a814c6eec4db4825e100172930e69a157928af53955a9'
