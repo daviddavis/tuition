a56c7799b4f88526e3d42df5cf26b72679782090
David Davis <davidd@scimedsolutions.com>
2012-03-11 13:31:40 -0400

    Setting all courses to active

--- db/migrate/20120303190918_add_active_columns.rb
+++ db/migrate/20120303190918_add_active_columns.rb
@@ -1,6 +1,13 @@
 class AddActiveColumns < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :students, :active, :boolean
     add_column :courses, :active, :boolean
+
+    Course.all.each {|c| c.update_attribute(:active, true)}
+  end
+
+  def self.down
+    remove_column :students, :active
+    remove_column :course, :active
   end
 end
